c75269a876e62e554ec0c4f8ff508c44e88f124d
DINESHKUMAR V <dineshvelu35@gmail.com>
2026-04-17 23:34:20 +0530

    Add files via upload

---
+++ climbing silver.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+  int N,C;
+  cin>>N>>C;
+  vector<string> grid(N);
+  for(int i=0;i<N;i++)
+  {
+    cin>>grid[i];
+  }
+  for(int i=N-1;i>=0;i--)
+  {
+  	for(int j=C-1;j>=0;j--)
+  	{
+  		
+	}
+  }
+}
+int main()
+{
+  int T;
+  cin>>T;
+  while(T--)
+  {
+    solve();
+  }
+}
